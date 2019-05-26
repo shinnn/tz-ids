9c7d59131d86132a17ba991b123641a15b8016bb
Watanabe Shinnosuke <snnskwtnb@gmail.com>
2019-05-26 19:41:56 +0900

    stop providing a browser global build

--- build.js
+++ build.js
@@ -1,25 +1,20 @@
 'use strict';
 
-const {join} = require('path');
-
 const {cyan, green} = require('chalk');
 const getTzIds = require('get-tz-ids');
 const rimrafPromise = require('rimraf-promise');
-const stringifyObject = require('stringify-object');
 const writeFileAtomically = require('write-file-atomically');
 
 require('loud-rejection/register');
 
 const pkg = require('./package.json');
-const browserMain = join(__dirname, 'browser.js');
 
-rimrafPromise(`{${pkg.files.join(',')},browser.js}`)
+rimrafPromise(`{${pkg.files.join(',')}`)
 .then(() => getTzIds())
 .then(ids => {
   return {
     'index.json': JSON.stringify(ids, null, '  ') + '\n',
-    [pkg['jsnext:main']]: 'export default ' + JSON.stringify(ids, null, '  ') + ';\n',
-    [browserMain]: `window.tzIds = ${stringifyObject(ids, {indent: '  '})};\n`
+    [pkg['jsnext:main']]: 'export default ' + JSON.stringify(ids, null, '  ') + ';\n'
   };
 })
 .then(files => {
--- test.js
+++ test.js
@@ -8,18 +8,12 @@ const expected = require('.');
 
 function runTest(description, main) {
   test(description, t => {
-    t.plan(1);
     t.deepEqual(main, expected, 'should expose an array of time zone IDs.');
+    t.end();
   });
 }
 
 rollup({entry: require('./package.json')['jsnext:main']}).then(bundle => {
   runTest('require(\'tz-ids\')', require('.'));
-
-  global.window = {};
-  require('./' + require('./bower.json').main);
-
-  runTest('window.tzIds', global.window.tzIds);
-
   runTest('Module exports', requireFromString(bundle.generate({format: 'cjs'}).code));
 });
